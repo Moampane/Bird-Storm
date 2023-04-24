c6b24b041b9f949d0f521c3ebc3514748e875ec1
Ellen Sun <sunellenyuan@gmail.com>
2023-04-24 17:40:09 -0400

    Add reference file for enemy testing

---
+++ reference_ellen.py
@@ -0,0 +1,55 @@
+import pygame
+from sys import exit
+from bird_class import *
+from enemy_class import *
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Bird Storm")
+clock = pygame.time.Clock()
+
+environment_surface = pygame.image.load("test_map_surface.jpg").convert()
+environment_surface = pygame.transform.scale(
+    environment_surface, (WINDOW_WIDTH, WINDOW_HEIGHT)
+)
+
+test_player = EnemyBird("graphics/penguin.png")
+test_enemies = []
+
+count = 0
+while True:
+    count += 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(environment_surface, (0, 0))
+
+    if count % 120 == 0:
+        test_bird = EnemyBird("graphics/duck.png")
+        test_enemies.append(test_bird)
+
+    for enemy in test_enemies:
+        enemy.draw(screen)
+    test_player.draw(screen)
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            test_player.sprite_rect.y -= 5
+        if event.key == pygame.K_DOWN:
+            test_player.sprite_rect.y += 5
+        if event.key == pygame.K_LEFT:
+            test_player.sprite_rect.x -= 5
+        if event.key == pygame.K_RIGHT:
+            test_player.sprite_rect.x += 5
+
+    if count > 100:
+        for enemy in test_enemies:
+            enemy.follow_player(test_player)
+        if count % 20 == 0:
+            for enemy in test_enemies:
+                enemy.attack(test_player)
+
+    pygame.display.update()
+    clock.tick(60)
