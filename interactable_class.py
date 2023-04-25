f6e6c0a2c816cd86a58eee812c27ad7b17b9d7f3
Ellen Sun <sunellenyuan@gmail.com>
2023-04-25 16:07:39 -0400

    Edit checking interaction method

--- interactable_class.py
+++ interactable_class.py
@@ -1,8 +1,12 @@
 """
 File for the Interactable class.
 """
-from abc import ABC, abstractmethod
 import pygame
+import random
+from abc import ABC, abstractmethod
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
 
 class Interactable(ABC):
@@ -14,6 +18,8 @@ class Interactable(ABC):
         containing the object's image
         _img: a pygame surface containing the image of the object
         _img_rect: a pygame rectangle mapped to the object image
+        _spawn_loc: a tuple containing the x and y spawn location of the
+        interactable
     """
 
     def __init__(self, img_path):
@@ -26,7 +32,13 @@ class Interactable(ABC):
         """
         self._img_path = img_path
         self._img = pygame.image.load(img_path).convert_alpha()
-        self._img_rect = pygame.transform.scale(self._img, (50, 50))
+        self._img = pygame.transform.scale(self._img, (50, 50))
+        self._img_rect = self._img.get_rect()
+        self._spawn_loc = (
+            random.randint([0, WINDOW_WIDTH - self._img_rect.width]),
+            random.randint(0, WINDOW_HEIGHT - self._img_rect.height),
+        )
+        self._img_rect = self._img.get_rect(center=self._spawn_loc)
 
     def check_interact(self, player):
         """
@@ -39,6 +51,7 @@ class Interactable(ABC):
             interactable or not.
         """
         if self._img_rect.colliderect(player.sprite_rect):
+            # CHANGE IMAGE TO HIGHLIGHT IN YELLOW AND DISPLAY PROMPT TO PICK UP
             return True
         return False
 
