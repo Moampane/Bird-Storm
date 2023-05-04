df86a3f159b31ee411c4ef984db11abebcf9c56c
Ellen Sun <sunellenyuan@gmail.com>
2023-05-04 16:37:33 -0400

    Edit player atk hitbox

--- sprite_atk_class.py
+++ sprite_atk_class.py
@@ -19,6 +19,8 @@ class Attack(pygame.sprite.Sprite):
         _animation_loop: An int representing the frame the animation is on.
         _first_hit: A boolean representing whether the first hit has
         occurred or not.
+        _txt_width: An int representing the width of the textbox of the attack
+        _txt_height: An int reprsenting the height of the textbos of the attack
     """
 
     def __init__(self, character, group):
@@ -35,6 +37,8 @@ class Attack(pygame.sprite.Sprite):
         self._character = character
         self._image = ATK_FONT.render("BONK", False, "Red")
         self._rect = self._image.get_rect(center=(-1000, -1000))
+        self._txt_width = self._rect.width
+        self._txt_height = self._rect.height
         self._animation_loop = 0
         self._first_hit = True
 
@@ -76,13 +80,19 @@ class Attack(pygame.sprite.Sprite):
         # different way of attacking, test if this is better?
         if self._character.is_facing_right():
             self._rect = self._image.get_rect(
-                topleft=(self._character.rect.topright)
+                center=(
+                    self._character.rect.topright[0] + self._txt_width / 2,
+                    self._character.rect.topright[1] + self._txt_height / 2,
+                )
             )
             self._rect.width = self._rect.width * 3
             self._rect.height = self._rect.height * 3
         else:
             self._rect = self._image.get_rect(
-                topright=self._character.rect.topleft
+                center=(
+                    self._character.rect.topleft[0] - self._txt_width / 2,
+                    self._character.rect.topleft[1] + self._txt_height / 2,
+                )
             )
             self._rect.width = self._rect.width * 3
             self._rect.height = self._rect.height * 3
