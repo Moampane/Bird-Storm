c0f9bfa2b2918682cb46af423e68ff25b39f92c0
Swasti Jain <swasti324@gmail.com>
2023-05-04 22:41:42 -0400

    Added more working tests (checking if the animation changes and hp changes when hit)

--- test_bird_class.py
+++ test_bird_class.py
@@ -1,11 +1,12 @@
 """
 Unit tests for overarching Bird Class
 """
-import pytest
-from sprite_bird_class import *
-from sprite_player_class import Player
-from sprite_test_game import *
 
+from sprite_bird_class import *
+from sprite_player_class import *
+
+# from sprite_test_game import *
+import pygame
 
 """
     Ideas:
@@ -14,18 +15,43 @@ from sprite_test_game import *
     check that the character's hp is the right width
     check character size is correct scale
 """
-test_path = "Animations/Philipe/Philipe_front_right_atk.png"
+test_path = "Animations/Steve/Steve_front_left_atk.png"
+# pygame.init()
+# clock = pygame.time.Clock()
+
+# Game Screen
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen1 = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 def test_check_hp():
     """
     Test that the character's remaining HP is set to the correct value after taking damage.
     """
-    player = Player(image_path= test_path)
-    initial_hp = player._remaining_hp
+    bird = Player(test_path, screen1)
+    initial_hp = bird._remaining_hp
     opponent_atk = 10
-    player.take_damage(opponent_atk)
-    assert player._remaining_hp == initial_hp - opponent_atk
+    bird.take_damage(opponent_atk)
+    assert bird._remaining_hp == initial_hp - opponent_atk
 
 
+def test_enemy_position():
+    """
+    The enemy should
+    """
 
+
+def test_animation():
+    """
+    Test that the character's animation is mapped to the correct image
+    """
+    bird = Player(test_path, screen1)
+    bird.is_facing_right() == True
+    bird.set_atk_status(True)
+    actual_img_path = bird.update_img()
+    expected_img_path = "Animations/Steve/Steve_front_right_atk.png"
+    assert actual_img_path == expected_img_path
+
+
+# write out every permutation
