fcf73726cf8a389b0e42ebc5c3c64c52b2ee97f8
Ellen Sun <sunellenyuan@gmail.com>
2023-05-05 00:30:03 -0400

    sprite_environment_class.py

--- sprite_test_game.py
+++ sprite_test_game.py
@@ -4,18 +4,18 @@ from sprite_player_class import *
 from sprite_environment_class import *
 from sprite_atk_class import *
 from sprite_bullet_class import *
+from start_game import intro_screen, reset_mc, reset_environment
 
 # Setup
 pygame.init()
 clock = pygame.time.Clock()
-BACKGROUND_IMG_PATH = "Animations/sky.png"
 
-# Game Screen
+BACKGROUND_IMG_PATH = "Animations/sky.png"
+PLAYER_IMG_PATH = "Animations/Steve/Steve_front_right_idle.png"
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# background = pygame.image.load("graphics/background.jpeg")
-# background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 environment = Environment(
     bg_path=BACKGROUND_IMG_PATH,
     bg_width=SCREEN_WIDTH,
@@ -24,26 +24,8 @@ environment = Environment(
 env_group = pygame.sprite.Group()
 env_group.add(environment)
 
-# intro screen
-WELCOME_FONT = pygame.font.Font("fonts/pixel.ttf", 95)
-START_FONT = pygame.font.Font("fonts/pixel.ttf", 50)
-background_img = pygame.image.load(BACKGROUND_IMG_PATH)
-background_img = pygame.transform.scale(
-    background_img, (SCREEN_WIDTH, SCREEN_HEIGHT)
-)
-welcome_txt = WELCOME_FONT.render("WELCOME TO BIRD GAME!", False, "Blue")
-welcome_txt_size = pygame.font.Font.size(WELCOME_FONT, "WELCOME TO BIRD GAME!")
-start_txt = START_FONT.render("Press Enter to start!", False, "Blue")
-start_txt_size = pygame.font.Font.size(START_FONT, "Press Enter to start!")
-controls_txt_w = START_FONT.render("W: Up", False, "Blue")
-controls_txt_a = START_FONT.render("A: Left", False, "Blue")
-controls_txt_s = START_FONT.render("S: Down", False, "Blue")
-controls_txt_d = START_FONT.render("D: Right", False, "Blue")
-controls_txt_space = START_FONT.render("Space: Attack", False, "Blue")
-controls_y_gap = start_txt_size[1] + 50
-
 # Player Group
-mc = Player("Animations/Steve/Steve_front_right_idle.png", screen)
+mc = Player(PLAYER_IMG_PATH, screen)
 player_group = pygame.sprite.Group()
 player_group.add(mc)
 
@@ -63,81 +45,43 @@ enemy_group = pygame.sprite.Group()
 boss_group = pygame.sprite.Group()
 
 timer = 0
-start_not_pressed = True
 
-while start_not_pressed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                start_not_pressed = False
-            if event.key == pygame.K_SPACE and pygame.sprite.Sprite.alive(mc):
-                player_atk = Attack(mc, player_atk_group)
-
-    screen.blit(background_img, (0, 0))
-    player_group.draw(screen)
-    player_group.update()
-    player_atk_group.draw(screen)
-    player_atk_group.update()
-
-    screen.blit(
-        welcome_txt,
-        (
-            SCREEN_WIDTH / 2 - welcome_txt_size[0] / 2,
-            SCREEN_HEIGHT / 3 - welcome_txt_size[1] / 2 - start_txt_size[1],
-        ),
-    )
-    screen.blit(
-        start_txt,
-        (
-            SCREEN_WIDTH / 2 - start_txt_size[0] / 2,
-            SCREEN_HEIGHT / 3,
-        ),
-    )
-
-    screen.blit(
-        controls_txt_w,
-        (
-            start_txt_size[0] / 3,
-            SCREEN_HEIGHT / 3 + controls_y_gap,
-        ),
-    )
-    screen.blit(
-        controls_txt_a,
-        (
-            start_txt_size[0] / 3,
-            SCREEN_HEIGHT / 3 + controls_y_gap * 1.5,
-        ),
-    )
-    screen.blit(
-        controls_txt_s,
-        (
-            start_txt_size[0] / 3,
-            SCREEN_HEIGHT / 3 + controls_y_gap * 2,
-        ),
-    )
-    screen.blit(
-        controls_txt_d,
-        (
-            start_txt_size[0] / 3,
-            SCREEN_HEIGHT / 3 + controls_y_gap * 2.5,
-        ),
-    )
-    screen.blit(
-        controls_txt_space,
-        (
-            start_txt_size[0],
-            SCREEN_HEIGHT / 3 + controls_y_gap,
-        ),
-    )
-    pygame.display.flip()
-    clock.tick(60)
-
+reset_game = False
 
 # Game Loop
-while not start_not_pressed:
+while True:
+    if environment.reset_game:
+        mc = reset_mc(screen)
+        environment = reset_environment()
+
+        # reset attack placeholders
+        player_atk = pygame.sprite.Sprite()
+        enemy_atk = pygame.sprite.Sprite()
+
+        # reset groups
+        player_atk_group = pygame.sprite.Group()
+        enemy_atk_group = pygame.sprite.Group()
+        boss_bullet_group = pygame.sprite.Group()
+        enemy_group = pygame.sprite.Group()
+        boss_group = pygame.sprite.Group()
+
+        environment.reset_game = False
+        timer = 0
+        environment.start_not_pressed = True
+
+    while environment.start_not_pressed:
+        intro_screen(
+            screen,
+            environment,
+            mc,
+            player_atk_group,
+            player_group,
+        )
+        player_group.add(mc)
+        env_group.add(environment)
+        pygame.display.flip()
+        clock.tick(60)
+
     timer += 1
     # Background
     env_group.draw(screen)
